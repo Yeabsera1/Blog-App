55b9ae896a5756f5db3fd9b22c5a0e806dfab985
Yeabsera1 <yeabmes01@gmail.com>
2024-07-10 10:46:17 +0200

    Day 2 work

---
+++ html-css-js/index.js
@@ -0,0 +1,9 @@
+console.log("hello world ");
+
+var fisrtName = "yeabsra "
+let lastName = "ababu"
+const fullName = fisrtName + lastName
+fullName += "updarte"
+console.log(fisrtName)
+console.log(lastName)
+console.log(fullName)
